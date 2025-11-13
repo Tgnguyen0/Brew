52c8b36d79a4a749b5510f76076422a5bcd35028
bichtram1402 <thanhpham.a2.2004@gmail.com>
2025-11-13 23:03:18 +0700

    Update statistic page + reload data

--- src/app/Listener/ActionListener_NavbarPanel.java
+++ src/app/Listener/ActionListener_NavbarPanel.java
@@ -15,29 +15,35 @@ public class ActionListener_NavbarPanel implements ActionListener {
 
         switch (command) {
             case "Trang Chủ":
-                // Khởi tạo trang Trang chủ
-//                if (DevCafeGUI.homePage != null) DevCafeGUI.homePage = new HomePage();
-
                 cardLayout.show(BrewGUI.pageContainer, "Home Page");
                 break;
-            case "Bán Hàng":
-                // Khởi tạo trang Bán hàng
-//                if (DevCafeGUI.sellPage != null) DevCafeGUI.sellPage = new SellPage();
 
+            case "Bán Hàng":
                 cardLayout.show(BrewGUI.pageContainer, "Sell Page");
                 break;
+
             case "Hóa Đơn":
                 cardLayout.show(BrewGUI.pageContainer, "Receipt Page");
                 break;
+
             case "Sản Phẩm":
                 cardLayout.show(BrewGUI.pageContainer, "Product Page");
                 break;
+
             case "Khách Hàng":
                 cardLayout.show(BrewGUI.pageContainer, "Customer Page");
                 break;
+
             case "Thống Kê":
+                // ⭐ Chuyển trang
                 cardLayout.show(BrewGUI.pageContainer, "Statistic Page");
+
+                // ⭐ Luôn cập nhật dữ liệu mới nhất từ Database
+                if (BrewGUI.statisticPage != null) {
+                    BrewGUI.statisticPage.reloadData();
+                }
                 break;
+
             case "Nhân Viên":
                 cardLayout.show(BrewGUI.pageContainer, "Employee Page");
                 break;
